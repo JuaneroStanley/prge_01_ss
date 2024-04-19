ed8691f455bf257643bf54f450d754f58a4c1d03
JuaneroStanley <stanislawskrzynskijan@gmail.com>
2024-04-19 10:55:13 +0200

    feat: poc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import "./App.css";
-
-const lista_uzytkownikow = [
-  { imie: "Kinga" },
-  { imie: "Mateusz" },
-  { imie: "Wiktoria" },
-  { imie: "Tomasz" },
-];
+import Home from "./components/home/Home";
 
 function App() {
+  const name = "Hello World!";
+
   return (
     <div className="App">
-      {lista_uzytkownikow.map((zolnierz) => {
-        return <div>{zolnierz.imie}</div>;
-      })}
+      <Home text={name} />
     </div>
   );
 }
