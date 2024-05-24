084f31b73a1f7026f5e8e3ee60379f6fb90e493f
JuaneroStanley <stanislawskrzynskijan@gmail.com>
2024-05-24 11:20:49 +0200

    wip

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,30 @@
-import React from "react";
-import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect } from "react";
+import {
+  LayersControl,
+  MapContainer,
+  TileLayer,
+  WMSTileLayer,
+} from "react-leaflet";
 import "./Map.css";
 import "leaflet/dist/leaflet.css";
+import { GeoJSON } from "react-leaflet";
+import axios from "axios";
 
 export default function Map() {
+  useEffect(() => {
+    console.log("Map component mounted");
+    const getData = () => {
+      axios
+        .get(
+          "http://127.0.0.1:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Agranice_wojewodztw_db&maxFeatures=50&outputFormat=application%2Fjson"
+        )
+        .then((dane) => {
+          console.log(dane.data + "dane");
+        });
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       <MapContainer className="map" center={[52.232222, 21.0]} zoom={6}>
@@ -16,6 +37,21 @@ export default function Map() {
           </LayersControl.BaseLayer>
           <LayersControl.BaseLayer name="OpenTopoMap">
             <TileLayer url="https://tile.opentopomap.org/{z}/{x}/{y}.png" />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Granice Wojewodztw">
+            <WMSTileLayer
+              url="http://127.0.0.1:8080/geoserver/prge/wms"
+              layers="granice_wojewodztw_db"
+              format="image/png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Granice Wojewodztw WFS">
+            {/* <LayersControl.Overlay checked name="Granice Wojewodztw WFS">
+              <GeoJSON
+                data={granice_wfs}
+                url="http://127.0.0.1:8080/geoserver/prge/wms"
+              />
+            </LayersControl.Overlay> */}
           </LayersControl.BaseLayer>
         </LayersControl>
       </MapContainer>
